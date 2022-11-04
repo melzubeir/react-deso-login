bd72256957b0171d418dfe84d1dc15f8918844a1
Mo Elzubeir <elzubeir@gmail.com>
2022-11-03 21:11:36 -0500

    even more basic

--- src/App.js
+++ src/App.js
@@ -1,85 +1,56 @@
-import './App.css';
-import React from "react";
-import BitcloutLogin from './BitcloutLogin.js'
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { makeStyles } from '@material-ui/core/styles';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
+import { StatusBar } from 'expo-status-bar';
+import Dialog from 'react-native-dialog';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MyDesoLogin from './components/MyDesoLogin';
 
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  loginButton: {
-    backgroundColor: '#FFFFFF',
-    textTransform: 'uppercase'
-  }
-}));
 
 function App() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [response, setResponse] = React.useState('');
+
+  const [open, setOpen] = useState(false);
+  const [response, setResponse] = useState('');
   const responseClout = (response) => {
     setResponse(JSON.stringify(response, null, 2));
     setOpen(true);
   }
-
+  console.log('func App(): response:', response);
   const handleClose = () => {
     setOpen(false);
   };
-  /**
-   * https://docs.bitclout.com/devs/identity-api#access-levels
-  */
+
   const accessLevel = 4;
-  /**
-   * JWT requires access leve 2+
-   * https://github.com/bitclout/identity/blob/main/src/app/identity.service.ts#L115
-   * https://github.com/bitclout/identity/blob/b4c4a041414778e8acb22d39afed5e7d72752178/src/types/identity.ts#L31
-   */
   const JWT = true;
+
   return (
-    <div className="App">
-      <div className={classes.paper}>
-        <BitcloutLogin
+    <>
+      <View style={styles.container}>
+        <MyDesoLogin
           accessLevel={accessLevel}
           onSuccess={responseClout}
           onFailure={responseClout}
-          JWT={JWT}
-          // customization={{className: classes.loginButton}}
-          // customIcon={<LockOpenIcon/>}
-        />
-      </div>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Bitclout auth response"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            <pre>
-              {response}
-            </pre>
-          </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          JWT={JWT} />
+        <Dialog.Container open={open}
+          onClose={handleClose}>
+          <Dialog.Title>Auth Response</Dialog.Title>
+          <Dialog.Description><pre>{response}</pre></Dialog.Description>
+          <Dialog.Button onClick={handleClose} >
             Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+          </Dialog.Button>
+        </Dialog.Container>
+      </View>
+
+      <StatusBar style="auto" />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
